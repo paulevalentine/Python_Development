5ec44563354b317e46e363d7afcb2cf439b7f391
paulevalentine <paul@the-valentines.co.uk>
2021-04-07 16:42:52 +0100

    work on snow load

---
+++ .ipynb_checkpoints/snow_test-checkpoint.py
@@ -0,0 +1,5 @@
+from snow import *
+a = Snow(20, 'duo', 3, 100)
+a.values()
+print(f'Ground snow load = {a.sk :.2f}')
+print(f'mu = {a.mu :.2f}')
--- snow.py
+++ snow.py
@@ -3,11 +3,14 @@ class Snow:
     """ Calculations in accordance with BS EN 1991-1-3 and NA """
     
     
-    def __init__(self, pitch=30, pitch_type='mono', sk=0.50):
+    def __init__(self, pitch=30, pitch_type='mono', Z=3, Alt = 100):
         """Form and instance of the class"""
         
         self.pitch_type = pitch_type
         self.pitch = pitch
+        self.Z = Z
+        self.Alt = Alt
+        
         
         # set the Ce value (exposure coeff NA 2.16)
         self.Ce = 1
@@ -27,7 +30,7 @@ class Snow:
             if self.pitch <= 15:
                 self.mu = 0.80
             elif 15 < self.pitch <= 30:
-                self.mu = 0.80 + 0.40*(1.2 - 15) / 15
+                self.mu = 0.80 + 0.40*(self.pitch - 15) / 15
             elif 40 < self.pitch <= 60:
                 self.mu = 1.2*(60 - self.pitch) / 30
             else:
@@ -35,7 +38,12 @@ class Snow:
         else:
             self.mu = 0.80 # end conservative number
             
-        self.s = self.mu * self.Ce * self.Ct
+        # calculate the value of the snow load on the ground 
+        self.sk = (0.15 + (0.1 * self.Z + 0.05) + ((self.Alt - 100) / 525))
+        
+        # calculate the roof snow load
+        self.s = self.mu * self.Ce * self.Ct * self.sk
+    
         
     def values(self):
         print(f'The snow load = {self.s :.2f}kPa')
