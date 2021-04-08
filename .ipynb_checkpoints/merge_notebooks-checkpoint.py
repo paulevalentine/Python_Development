548f0416985075ab2782d58d0adea0e37da3a5aa
paulevalentine <paul@the-valentines.co.uk>
2021-04-08 14:35:45 +0100

    testing stuff

--- .ipynb_checkpoints/merge_notebooks-checkpoint.py
+++ .ipynb_checkpoints/merge_notebooks-checkpoint.py
@@ -6,10 +6,11 @@ import nbformat as nbf
 intro = nbf.read('calculation_intro.ipynb', 4)
 snow = nbf.read('snow_loading.ipynb', 4)
 loading = nbf.read('loading.ipynb', 4)
+beam = nbf.read('beam_analysis.ipynb', 4)
 
 # set the pack meta data and merge the cells
 pack = nbf.v4.new_notebook(metadata=snow.metadata)
-pack.cells = intro.cells + snow.cells + loading.cells
+pack.cells = intro.cells + snow.cells + loading.cells + beam.cells
 
 # set the pack name and write back the combined book.
 nbf.write(pack, 'pack.ipynb')
